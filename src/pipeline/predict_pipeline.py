8d22ffb2fe6d4ce7848cfed4ed9eebe520eb0851
Abhilash-0322 <mauryaabhi2003@gmail.com>
2025-09-09 19:53:16 +0530

    fix

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -44,12 +44,12 @@ class CustomData:
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
-                "race/ethnicity":[self.race_ethnicity],
-                "parental level of education":"none",
-                "lunch":[self.lunch],
-                "test preparation course":"none",
-                "reading score":[self.reading_score],
-                "writing score":[self.writing_score]
+                "race/ethnicity": [self.race_ethnicity],
+                "parental level of education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test preparation course": [self.test_preparation_course],
+                "reading score": [self.reading_score],
+                "writing score": [self.writing_score]
             }
             df = pd.DataFrame(custom_data_input_dict)
             if df.isnull().any().any():
